efb984acaac2c8b4202d5353112de4b51594a545
mikee47 <mike@silyhouse.net>
2024-06-03 12:14:22 +0100

    Validate parser output

--- test/app/parser.cpp
+++ test/app/parser.cpp
@@ -1,6 +1,11 @@
 #include <SmingTest.h>
 #include <CSV/Parser.h>
 #include <malloc_count.h>
+#include <WVector.h>
+
+// 0: Normal operation
+// 1: Generate output comparison files (host only)
+#define GENERATE_OUT_FILES 0
 
 namespace
 {
@@ -48,6 +53,15 @@ private:
 	void parseFile(const String& filename, char sep, Mode mode)
 	{
 		this->mode = mode;
+		lines.clear();
+
+#if GENERATE_OUT_FILES
+		auto& fs = IFS::Host::getFileSystem();
+		out = IFS::File(&fs);
+		CHECK(out.open(F("files/") + filename + ".out", File::CreateNewAlways | File::WriteOnly));
+#else
+		CHECK(out.open(filename + ".out"));
+#endif
 
 		Serial << endl << _F(">> Parse file '") << filename << '\'' << endl;
 
@@ -80,22 +94,66 @@ private:
 			auto elapsed = timer.elapsedTicks();
 			printHeap();
 			Serial << F("Elapsed ticks ") << elapsed << endl;
+			return;
+		}
+
+		for(auto& cursor : lines) {
+			file.seek(cursor.start, SeekOrigin::Start);
+			String s;
+			s.setLength(cursor.length());
+			CHECK(file.read(s.begin(), s.length()) == s.length());
+			// m_printHex("LINE", s.c_str(), s.length());
+			Serial << s << endl;
 		}
 	}
 
 	bool handleRow()
 	{
 		auto& row = parser->getRow();
-		auto cursor = parser->getCursor();
+		auto& cursor = parser->getCursor();
+
+#if GENERATE_OUT_FILES
+		uint16_t len = row.length();
+		out.write(&len, sizeof(len));
+		out.write(row.c_str(), len);
+#else
+		uint16_t len;
+		CHECK(out.read(&len, sizeof(len)) == sizeof(len));
+		String s;
+		s.setLength(len);
+		CHECK(out.read(s.begin(), s.length()) == len);
+		CHECK(row == s);
+#endif
+
+#if 0
+		if(mode != Mode::timed) {
+			/*
+			 * Read raw line data from file and print it
+			 *
+			 * TODO: Can we do any validation here?
+			 */
+			auto oldPos = file.seek(0, SeekOrigin::Current);
+			file.seek(cursor.start, SeekOrigin::Start);
+			String s;
+			s.setLength(cursor.length());
+			CHECK(file.read(s.begin(), s.length()) == s.length());
+			file.seek(oldPos, SeekOrigin::Start);
+			// m_printHex("LINE", s.c_str(), s.length());
+			Serial << s << endl;
+		}
+#endif
+
 		switch(mode) {
 		case Mode::print:
 			Serial << "@" << cursor << " " << row.count() << " COLS: " << row.join(", ") << endl;
+			lines.add(cursor);
 			break;
 		case Mode::dump:
 			Serial << cursor << " " << row.count() << " COLS:" << endl;
 			for(auto cell : row) {
 				m_printHex("  CELL", cell, strlen(cell));
 			}
+			lines.add(cursor);
 			break;
 		case Mode::timed:
 			break;
@@ -105,9 +163,11 @@ private:
 	}
 
 	File file;
+	File out;
 	std::unique_ptr<CSV::Parser> parser;
 	size_t totalRowSize{0};
 	Mode mode{};
+	Vector<CSV::Cursor> lines;
 };
 
 void REGISTER_TEST(parser)
